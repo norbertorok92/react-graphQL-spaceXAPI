df8c96795f36ac863e6254e54c5d64aba3b3fca9
Norbert Torok <ntorok@grubhub.com>
2020-07-15 15:32:57 +0300

    push UI

---
+++ client/src/components/Launches.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { useQuery } from '@apollo/react-hooks'
+import LaunchItem from './LaunchItem'
+
+const LAUNCHES_QUERY = gql`
+    query LaunchesQuery {
+        launches {
+            flight_number
+            mission_name
+            launch_date_local
+            launch_success
+        }
+    }
+`
+
+export default function Launches() {
+    const { loading, error, data } = useQuery(LAUNCHES_QUERY)
+
+    if (loading) return <h4>Loading...</h4>
+    if (error) console.log(error)
+
+    return (
+        <div>
+            <h1 className="display-4 my-3">Launches</h1>
+            {
+                data && data.launches.map(launch => (
+                    <LaunchItem key={launch.flight_number} launch={launch} />
+                ))
+            }
+        </div>
+    )
+}
